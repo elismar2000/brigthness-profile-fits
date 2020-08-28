74909a31d31bf7abb9abc0c121d1c000c2c31cf3
Elismar Lösch <elismar@astro.ufsc.br>
2020-08-28 15:54:18 -0300

    commiting everything inside the imfit folder

---
+++ codes/sersic_to_hernquist2.py
@@ -0,0 +1,49 @@
+import abel
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+
+from photutils.isophote import EllipseGeometry, Ellipse
+
+#=======================
+#Performing the inverse Abel transformation over the bulge 2D image
+#=======================
+
+bulge_image = '/home/elismar/Documentos/Fisica/IC/imfit-1.7.1/ngc2992-93/images/NICMOS/n4sb08040/bulge_cut.fits'
+image = fits.getdata(bulge_image)
+
+#The center pixel coordinates of the bulge
+x0 = 15
+y0 = 16
+bulge_3D = abel.Transform(image, direction='inverse', method='three_point', center=(y0, x0)).transform
+
+#=======================
+#Fitting ellipses to the 3D image in order to get its brigthness profile
+#=======================
+
+geometry = EllipseGeometry(x0=14, y0=14, sma=2, eps=0.07, pa=0)
+
+ellipse = Ellipse(bulge_3D, geometry)
+isolist = ellipse.fit_image()
+
+#========================
+#Plot results
+#========================
+
+fig, (ax1, ax2) = plt.subplots(figsize=(14, 5), nrows=1, ncols=2)
+norm = plt.Normalize(-2, 2)
+ax1.imshow(bulge_3D, origin='lower', norm=norm)
+ax1.set_title('Data')
+
+smas = np.linspace(1, 10, 5)
+for sma in smas:
+    iso = isolist.get_closest(sma)
+    x, y, = iso.sampled_coordinates()
+    ax1.plot(x, y, color='white')
+
+ax2.plot(isolist.sma, isolist.intens, color='purple', marker='x')
+ax2.set_xlabel(r'$sma\ [pixel]$')
+ax2.set_ylabel(r'$countrate\ [DN\ sec^{-1}]$')
+ax2.set_title('Brigthness Profile')
+
+plt.show()
